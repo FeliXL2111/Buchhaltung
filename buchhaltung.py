07b6fe14e64fbcc30c2eb4778f5d393455d62552
Felix <febernard@web.de>
2022-08-18 15:27:01 +0200

    plots speichern

--- buchhaltung.py
+++ buchhaltung.py
@@ -1,5 +1,4 @@
 import json
-import matplotlib
 from tkinter import *
 from tkinter.font import *
 
@@ -7,9 +6,12 @@ window = Tk()
 window.title('Buchhaltung')
 window.iconbitmap('icon.ico')
 window.geometry('1000x1000')
+window.configure(bg='grey26')
 
 
-
+photo = PhotoImage(file=r'C:\Users\Felix\VS Code Projekts\Buchhaltung\plots\first_plot.png')
+l_photo = Label(window, image=photo)
+l_photo.pack(pady=5)
 
 
 
--- plot.py
+++ plot.py
@@ -1,1 +1,9 @@
+import matplotlib.pyplot as plt
 import json
+
+x = [1,2,3,4]
+y = [2,4,3,1]
+
+plt.plot(x, y)
+plt.savefig(r'C:\Users\Felix\VS Code Projekts\Buchhaltung\plots\first_plot.png')
+plt.show()
