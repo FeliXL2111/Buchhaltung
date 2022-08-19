ca0e6847f6a4fb387c00d8d925956e337a9d0f24
Felix <febernard@web.de>
2022-08-19 13:10:37 +0200

    theoretisch reloden

--- plot.py
+++ plot.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt
 import json
 
 x = [1,2,3,4]
-y = [4,2,4,0]
+y = [4,1,4,0]
 
 plt.plot(x, y)
 path = r'C:\Users\Felix\VS Code Projekts\Buchhaltung\plots\first_plot.png'
