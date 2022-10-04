ab747e5cf5eaff43bdd6353f6c81baf02a12a4fa
FeliXL2111 <febernrad@web.de>
2022-10-04 16:41:37 +0200

    compare

--- data_processing/date_comparison.py
+++ data_processing/date_comparison.py
@@ -1,36 +1,39 @@
 def comparison(date1, date2):
     date1 = date1.split('_')
-    for number in date1:
-        int(number)
     date2 = date2.split('_')
+    for i in range(0, len(date1)):
+        date1[i] = int(date1[i])
+    for i in range(0, len(date2)):
+        date2[i] = int(date2[i])
 
+    print(date1, date2)
+    
     day = 0
-    
     month = 1
-    
     year = 2
     
 
     if date1[year] > date2[year]:
-        return 'first later'
+        return 'first closer to today, year'
     elif date1[year] == date2[year]:
 
         if date1[month] > date2[month]:
-            return 'first later'
+            return 'first closer to today, month'
         elif date1[month] == date2[month]:
 
             if date1[day] > date2[day]:
-                return 'first later'
+                return 'first closer to today, day'
             elif date1[day] == date2[day]:
                 return 'eqaul'
             else:
-                return 'first earlyer'
+                return 'second closer to today, day'
 
         else:
-            return 'first earlyer'
+            return 'second closer to today, month'
         
     else:
-        return 'first earlyer'
+        return 'second closer to today, year'
+
     
 
-print(comparison('02_11_2017', '02_11_2021'))
+print(comparison('02_10_2022', '02_11_2022'))
