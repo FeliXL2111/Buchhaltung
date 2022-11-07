d3d3b0bc8fb8a74a38db56f2382031f47c154283
FeliXL2111 <febernrad@web.de>
2022-11-07 19:39:29 +0100

    admin sql

--- sourse/sql_probe.py
+++ sourse/sql_probe.py
@@ -13,8 +13,45 @@ def add_to_tabel(user, atribure_list):
 def print_tabel(user):
     with sqlite3.connect('../user/'+user.lower_name+'/sql_data.db') as database:
         for i in database.execute("select * from data"):
-            print(i) 
+            print(i)
 
-# create_table('name')
-# add_to_tabel("humans", ("Gregor", 300))
-# print_tabel(None)
+
+
+def create_table_admin():
+    with sqlite3.connect('user/admin/sql_data.db') as database:
+        database.execute("CREATE TABLE data (amount float, bankkonto float, day int, month int, year int, info text)")
+    
+def add_to_tabel_admin(atribure_list):
+    with sqlite3.connect('user/admin/sql_data.db') as database:
+        database.execute(f"INSERT INTO data VALUES {atribure_list}")
+
+def print_tabel_admin():
+    with sqlite3.connect('user/admin/sql_data.db') as database:
+        for i in database.execute("select * from data"):
+            print(i)
+
+def delete_from_table_admin():
+    with sqlite3.connect('user/admin/sql_data.db') as database:
+        database.execute("delete from data WHERE day = 2 AND amount = 20")
+
+def add_new_colum_admin():
+    with sqlite3.connect('user/admin/sql_data.db') as database:
+        database.execute("ALTER TABLE data ADD transsaction_id int")
+
+if __name__ == '__main__':
+    while True:
+        sql_que = input('Gib deine Aktion an: ')
+        if sql_que == 'ct':
+            tmp = input('Name des table')
+            create_table_admin(tmp)
+        if sql_que == 'att':
+            tmp = input('')
+            add_to_tabel_admin()
+        if sql_que == 'pt':
+            print_tabel_admin()
+        if sql_que == 'dft':
+            delete_from_table_admin()
+        if sql_que == 'anc':
+            add_new_colum_admin()
+        if sql_que == 'quit':
+            break
