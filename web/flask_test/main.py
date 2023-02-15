24c8ab5f75df568bd76c6c65ac3109fc318612d8
Felix <febernard@web.de>
2023-02-15 21:14:22 +0100

    more flask

--- web/flask_test/main.py
+++ web/flask_test/main.py
@@ -3,7 +3,7 @@ from views import views
 
 app = Flask(__name__)
 
-app.register_blueprint(views, url_prefix="/views")
+app.register_blueprint(views, url_prefix="/")
 
 if __name__ == '__main__':
     app.run(debug=True, port=8000, host="192.168.2.119")
--- web/flask_test/views.py
+++ web/flask_test/views.py
@@ -1,7 +1,11 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 views = Blueprint(__name__, "views")
 
 @views.route("/")
 def home():
-    return "home page"
+    return render_template("index.html", name="Tim")
+
+@views.route("/profile/<username>")
+def profile(username):
+    return render_template("index.html", name=username)
