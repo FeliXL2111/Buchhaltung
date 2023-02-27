51a861a590ab0ae04e37e6309144b1c2c340b04e
FeliXL2111 <febernrad@web.de>
2023-02-27 17:27:56 +0100

    backend , todo

--- backend/backend.cpp
+++ backend/backend.cpp
@@ -1,5 +1,20 @@
 #include <iostream>
+#include <stdlib.h>
 
 int main(){
-    std::cout << "backend läuft";
+    std::cout << "backend läuft" << std::endl;
+    std::cout << system("curl https://198.168.2.119:8008") << std::endl;
+    try
+    {
+        int m;
+        m = system("curl https://198.168.2.119:8008");
+        std::cout << m << std::endl;
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << '\n';
+    }
+
+    
+    // std::cout << i << std::endl;
 }
